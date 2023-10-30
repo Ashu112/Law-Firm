f531bd9a6c5c6d7c18ed3c60d16eb02325c667c3
Ashu112 <kashu2611@gmail.com>
2023-10-30 22:54:03 +0530

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+import LogoImage from '../../assets/Logo.png'
+const Navbar = () => {
+  return (
+    <div className={styles.wrapper}>
+       <img src={LogoImage} alt="logo"/>
+       <div className={styles.navHead}>
+        <p>Home</p>
+        <p>Attorneys</p>
+        <p>PracticeAreas</p>
+        <p>About Us</p>
+        <p></p>
+       </div>
+       <button className={styles.btn}>Contact Now</button>
+    </div>
+  )
+}
+
+export default Navbar
