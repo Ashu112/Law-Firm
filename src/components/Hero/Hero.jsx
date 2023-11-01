cf3bb1590275b100f7722faf16c9523fdef59ae2
Ashu112 <kashu2611@gmail.com>
2023-11-01 17:30:45 +0530

    hero complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,16 +7,24 @@ const Hero = () => {
     <div>
       <div className={styles.wrapper}>
         <div className={styles.heroText}>
-          <div>
-            <h1>You don’t have to </h1>
-            <h1>Fight them Alone.</h1>
+          <div className={styles.heading}>
+            <h1 style={{ fontSize: "50px", fontWeight: "500" }}>
+              You don’t have to{" "}
+            </h1>
+            <h1 style={{ fontSize: "65px", fontWeight: "700" }}>
+              Fight them Alone.
+            </h1>
           </div>
-          <p>
+          <p className={styles.HeroPara}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
             iste ab! Libero repellendus aut dolore, alias totam id inventore
             laudantium reprehenderit perferendis. Assumenda ad nisi vel omnis
             doloribus a sapiente?
           </p>
+          <form className={styles.heroBar}>
+            <input className={styles.search} type="text" placeholder="Enter your eamil address"></input>
+            <button className={styles.button}>Let’s Talk</button>
+          </form>
         </div>
         <img src={HeroImage} alt="Hero" />
       </div>
